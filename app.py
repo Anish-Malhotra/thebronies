47c0a16f67fc0ba15eaf7d6592dd44b7db68fdd6
Jerry Dai <jdai315@gmail.com>
2014-11-10 11:45:43 -0500

    yay

--- app.py
+++ app.py
@@ -4,16 +4,28 @@ import json
 
 app = Flask(__name__)
 
-url = """http://ponyfac.es/api.json/tag:happy"""
+url = """
+http://api.brewerydb.com/v2/beer/?key=<MYKEY>&format=json
+"""
+
+url1 ="""
+http://api.brewerydb.com/v2/location/d25euf/?key=0c040a3ebf5d18acb0162c76cea5ebc9
+"""
+
+url2 ="""
+http://ponyfac.es/api.json/tag:happy
+"""
+
 
 @app.route("/")
 def home():
-    request = urllib2.urlopen(url)
+    request = urllib2.urlopen(url1)
     resultstring = request.read()
     result = json.loads(resultstring)
-    return result
+    return resultstring
+   # return str(result["timestamp"])
 
 if __name__ == "__main__":
     app.debug = True
-    app.run(host="0.0.0.0", port=1000)
+    app.run(host="0.0.0.0", port=4321)
 

31bdf0dbf58b4d3d28fa166f314d0864bd1e1820
unknown <jdai315@gmail.com>
2014-11-11 19:50:46 -0500

    basic flask stuff, input type is beer IDs

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
-from flask import Flask
-import urllib2
-import json
+from flask import Flask, render_template, request
+import urllib2, json
 
 app = Flask(__name__)
-
-url = """
-http://api.brewerydb.com/v2/beer/?key=<MYKEY>&format=json
-"""
 
 url1 ="""
 http://api.brewerydb.com/v2/location/d25euf/?key=0c040a3ebf5d18acb0162c76cea5ebc9
@@ -16,14 +11,24 @@ url2 ="""
 http://ponyfac.es/api.json/tag:happy
 """
 
+@app.route("/", methods=["GET","POST"])
+def home():
+    if request.method == "GET":
+        return render_template("home.html")
+    else:
+        search = request.form["search"]
+        button = request.form["b"]
+        if button == None:
+            return render_template("home.html")
+        else:
+            return page(search)
 
 @app.route("/")
-def home():
-    request = urllib2.urlopen(url1)
+def page(search=None):
+    url = "http://api.brewerydb.com/v2/beer/" + search + "/?key=0c040a3ebf5d18acb0162c76cea5ebc9"
+    request = urllib2.urlopen(url)
     resultstring = request.read()
-    result = json.loads(resultstring)
-    return resultstring
-   # return str(result["timestamp"])
+    return render_template("page.html", result = resultstring)
 
 if __name__ == "__main__":
     app.debug = True
